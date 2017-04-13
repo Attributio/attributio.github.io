13102dd2c8d8d74ca111679845b30470d0a59803
David Chun <daveedchun@gmail.com>
2017-04-13 16:05:20 -0700

    added clientid to event tracking

--- js/track.js
+++ js/track.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
   $(".track").click(function(){
 
     if (typeof ga === 'function') {
-      ga('send', {
-        hitType: 'event',
-        eventCategory: $(this).data("event-category"),
-        eventAction: $(this).data("event-action"),
-        eventLabel: $(this).data("event-label") + ': ' + $(this).text()
+      ga(function(tracker) {
+        var clientId = tracker.get('clientId');
+        ga('send', {
+          hitType: 'event',
+          eventCategory: $(this).data("event-category"),
+          eventAction: $(this).data("event-action"),
+          eventLabel: $(this).data("event-label") + ': ' + $(this).text(),
+          dimension1: clientId
+        });
       });
 
     }
