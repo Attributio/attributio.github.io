58b0d8d331de2e806c4097566ef0b02b3576bcf7
David Chun <daveedchun@gmail.com>
2018-11-27 08:31:52 -0800

    modified support page and tracking trigger

--- js/track.js
+++ js/track.js
@@ -1,22 +1,23 @@
-$(document).ready(function() {
+// Need to reset based on GTM cookie
+// $(document).ready(function() {
 
-  $(".track").click(function(){
+//   $(".track").click(function(){
 
-    if (typeof ga === 'function') {
-      ga(function(tracker) {
-        var clientId = tracker.get('clientId');
-        ga('send', {
-          hitType: 'event',
-          eventCategory: $(this).data("event-category"),
-          eventAction: $(this).data("event-action"),
-          eventLabel: $(this).data("event-label") + ': ' + $(this).text(),
-          dimension1: clientId
-        });
-      });
+//     if (typeof ga === 'function') {
+//       ga(function(tracker) {
+//         var clientId = tracker.get('clientId');
+//         ga('send', {
+//           hitType: 'event',
+//           eventCategory: $(this).data("event-category"),
+//           eventAction: $(this).data("event-action"),
+//           eventLabel: $(this).data("event-label") + ': ' + $(this).text(),
+//           dimension1: clientId
+//         });
+//       });
 
-    }
+//     }
 
-    return true;   
-  });
+//     return true;   
+//   });
 
-});
+// });
